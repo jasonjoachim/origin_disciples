bacb88144e3c0a61b7d288c2ca627f5d9b0be1d6
Mark Strelecky <streleck@gmail.com>
2017-08-24 13:28:01 -0500

    displayGIF function

--- gif_display.js
+++ gif_display.js
@@ -4,37 +4,27 @@ var apikey = "60a662cf5d774be4922ed09719bdb709";
 var gifSet = [];
 var repeat;
 
-$("#get-gif-btn").on("click", function(){
-  event.preventDefault();
+function displayGIF(){
+  var query = $("#emo-input").val().trim();
 
   var queryURL = "https://"
                   +host
                   +path
                   +"?q=" + query
                   +"&api_key=" + apikey
-
-  var gifSet = [];
+  //loop through ajax requests to get 10 images
   for (i=0; i<10; i++){ 
     
     $.ajax({
     url: queryURL,
     method: "GET"
     }).done(function(response){
-      // console.log(response);
-
-      // console.log("the gif: " + response.data.image_original_url)
-
-    var newGif = response.data.image_original_url; 
-    gifSet.push(newGif);  
+     
+    //display each
+    var newGif = $("<img>")
+    gif.attr("src", response.data.image_original_url);
+    gif.attr("class", "gif");
+    $("#gif-dump").append(gif);
     });
- 
-
-function gifDisplay(gifArray){
-
-  console.log("array: " + gifArray);  
-
-  for (j=0; j < gifArray.length; j++){
-    console.log("looping");
-    $("#gif-dump").append($("<img>").attr("src", gifArray[j]));
-  };
+  }
 };
